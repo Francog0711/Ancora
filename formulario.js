f95467872bb842ec5d7faec225c599b9bf6d8037
Francog0711 <francogabrielalconchel@gmail.com>
2023-12-14 05:00:30 -0300

    form api

--- formulario.js
+++ formulario.js
@@ -69,8 +69,7 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
+formulario.addEventListener('submit', () => {
 
 	const terminos = document.getElementById('terminos');
 	if(campos.apellido && campos.nombre && campos.provincia && campos.correo && campos.telefono && campos.ciudad && terminos.checked ){
