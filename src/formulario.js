fcc6513438073abb4096a50dba1a4fe57de85ba2
Francog0711 <francogabrielalconchel@gmail.com>
2023-12-15 14:28:05 -0300

    formulario limpio

--- src/formulario.js
+++ src/formulario.js
@@ -4,10 +4,10 @@ const inputs = document.querySelectorAll("#formulario-contacto input");
 
 
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    ciudad: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    provincia: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+	apellido: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+    ciudad: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
+    provincia: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
@@ -69,19 +69,3 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-// 	const terminos = document.getElementById('terminos');
-// 	if(campos.apellido && campos.nombre && campos.provincia && campos.correo && campos.telefono && campos.ciudad && terminos.checked ){
-// 		formulario.reset();
-
-// 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-// 		setTimeout(() => {
-// 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-// 		}, 5000);
-
-// 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-// 			icono.classList.remove('formulario__grupo-correcto');
-// 		});
-// 	} else {
-// 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-// 	}
-// });
